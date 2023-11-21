69d960218a030d36f09de638d7ccd916c61dfcc8
EdoBergamo <bergamo@edoardo-hb.eu>
2023-11-21 16:40:27 +0100

    [UPDATE] Display Data in HTML

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 let DEBUG = true; // Setup DEBUG to see every console.log
 
 // Inizializzazione Varabili
+const nome = document.getElementById('name');
 const km = document.getElementById('km');
 const eta = document.getElementById('eta')
 const button = document.getElementById('submit');
@@ -9,7 +10,12 @@ const prezzoPerKm = 0.21;
 const scontoOver65 = 40;
 const scontoUnder18 = 20;
 
+let bigliettoElement = document.getElementById("biglietto");
+let nomePasseggero = document.getElementById("fname");
+let costoBiglietto = document.getElementById("costo-biglietto");
+
 button.addEventListener("click", function () {
+    bigliettoElement.style.display = "block"
     let biglietto = km.value * prezzoPerKm;
     
     if (eta.value == "minorenne") {
@@ -17,15 +23,19 @@ button.addEventListener("click", function () {
     } else if (eta.value == "over65") {
         biglietto -= (biglietto * scontoOver65) / 100;
     }
-    
+
+    nomePasseggero.innerHTML += nome.value;
+    costoBiglietto.innerHTML += biglietto.toFixed(2);
+
     if (DEBUG) {
-        console.log(km.value, eta.value);
-        console.log(biglietto.toFixed(2))
+        console.log(km.value, eta.value, nome.value);
+        console.log(biglietto.toFixed(2));
     }
 })
 
 // Se DEBUG == true allora fai tutti i console log
 if (DEBUG) {
+    console.log(nome)
     console.log(km);
     console.log(eta);
     console.log(button);
