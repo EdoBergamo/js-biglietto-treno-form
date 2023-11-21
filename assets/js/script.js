ede1224a0ee69a1f10a3e70a88706ddca532e671
EdoBergamo <bergamo@edoardo-hb.eu>
2023-11-21 16:17:43 +0100

    [UPDATE] Added logic for discount

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,9 +5,22 @@ const km = document.getElementById('km');
 const eta = document.getElementById('eta')
 const button = document.getElementById('submit');
 
+const prezzoPerKm = 0.21;
+const scontoOver65 = 40;
+const scontoUnder18 = 20;
+
 button.addEventListener("click", function () {
+    let biglietto = km.value * prezzoPerKm;
+    
+    if (eta.value < 18) {
+        biglietto -= (biglietto * scontoUnder18) / 100;
+    } else if (eta.value >= 65) {
+        biglietto -= (biglietto * scontoOver65) / 100;
+    }
+
     if (DEBUG) {
         console.log(km.value, eta.value);
+        console.log(biglietto.toFixed(2))
     }
 })
 
